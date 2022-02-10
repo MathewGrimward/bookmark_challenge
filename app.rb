a689238ab57546c9e34d0efaf97065092c3df4f9
MathewGrimward <mathewgrimward@gmail.com>
2022-02-10 16:16:46 +0000

    Challenge 10 complete

--- app.rb
+++ app.rb
@@ -23,9 +23,10 @@ class BookmarkManager < Sinatra::Base
     end
 
     post '/bookmarks' do
-        url = params['url']
-        connection = PG.connect(dbname: 'bookmark_manager_test')
-        connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+        # url = params['url']
+        # connection = PG.connect(dbname: 'bookmark_manager_test')
+        # connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+        Bookmark.create(url: params[:url])
         redirect '/bookmark'
     end
 
