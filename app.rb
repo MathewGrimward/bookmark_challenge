bc26d7cab17c792ce5837cbb9fc84f18865eac72
MathewGrimward <mathewgrimward@gmail.com>
2022-02-10 16:02:18 +0000

    Challenge 10 - up to Refactoring

--- app.rb
+++ app.rb
@@ -13,8 +13,20 @@ class BookmarkManager < Sinatra::Base
     end
 
     get '/bookmark' do
+        #p ENV
         @bookmarks = Bookmark.all
         erb :'bookmarks/index'
+    end
+
+    get '/bookmark/new' do
+        erb :"bookmarks/new"
+    end
+
+    post '/bookmarks' do
+        url = params['url']
+        connection = PG.connect(dbname: 'bookmark_manager_test')
+        connection.exec("INSERT INTO bookmarks (url) VALUES('#{url}')")
+        redirect '/bookmark'
     end
 
     run! if app_file == $0
